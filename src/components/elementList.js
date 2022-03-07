505a66065d71839709ed3d88670aac2012ce2146
Łukasz Strama <strama.lukasz54@gmail.com>
2022-03-07 21:51:43 +0100

    Update elementList.js

--- src/components/elementList.js
+++ src/components/elementList.js
@@ -9,7 +9,7 @@ export default class ElementList extends React.Component {
                     {this.props.yearCard==='2021' && 
                         <div className="element-list-container">
                             {table.map((book)=>(
-                            <div className="element" key={book.book_name} style={{display: (book.book_name === ' ' && 'none')}}>
+                            <div className="element" key={book.book_name} style={{display: + (book.book_releaseYear === 2022 && 'none')}}>
                                 <div className="element-img"><img src={book.book_releaseYear === 2021 ? book.book_photography : undefined} alt="img"/></div>
                                 <div className="element-title">{book.book_releaseYear === 2021 && book.book_name}</div>
                             </div>
@@ -20,7 +20,7 @@ export default class ElementList extends React.Component {
                     {this.props.yearCard==='2022' && 
                         <div className="element-list-container">
                             {table.map((book)=>(
-                            <div className="element" key={book.book_name} style={{display: (book.book_name === ' ' && 'none')}}>
+                            <div className="element" key={book.book_name} style={{display: + (book.book_name === ' ' && 'none')}}>
                                 <div className="element-img"><img src={book.book_releaseYear === 2022 ? book.book_photography: undefined} alt="img"/></div>
                                 <div className="element-title">{book.book_releaseYear === 2022 && book.book_name}</div>
                             </div>
