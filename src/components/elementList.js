98d85c4fe5db5f9b0b6ef42c4db097514429aec5
Łukasz Strama <strama.lukasz54@gmail.com>
2022-03-10 12:47:50 +0100

    final

--- src/components/elementList.js
+++ src/components/elementList.js
@@ -6,28 +6,39 @@ export default class ElementList extends React.Component {
     render() {
         return(
             <div>
-                    {this.props.yearCard==='2021' && 
+                    {this.props.yearCard==='2021' &&
                         <div className="element-list-container">
-                            {table.map((book)=>(
-                            <div className="element" key={book.book_name} style={{display: + (book.book_releaseYear === 2022 && 'none')}}>
-                                <div className="element-img"><img src={book.book_releaseYear === 2021 ? book.book_photography : undefined} alt="img"/></div>
-                                <div className="element-title">{book.book_releaseYear === 2021 && book.book_name}</div>
-                            </div>
-                            ))} 
+                            {table.map((book) => (book.book_releaseMonth === 1 && this.outRow(2021)))}
                         </div>
                     }
 
                     {this.props.yearCard==='2022' && 
                         <div className="element-list-container">
-                            {table.map((book)=>(
-                            <div className="element" key={book.book_name} style={{display: + (book.book_name === ' ' && 'none')}}>
-                                <div className="element-img"><img src={book.book_releaseYear === 2022 ? book.book_photography: undefined} alt="img"/></div>
-                                <div className="element-title">{book.book_releaseYear === 2022 && book.book_name}</div>
-                            </div>
-                            ))} 
+                            {table.map((book) => (book.book_releaseMonth === 1 && this.outRow(2020)))}
                         </div>
                     } 
             </div>
       );
     }
+
+    outRow(year) {
+        return <div className="element-list-row">
+            {table.filter(book => {
+                if (this.props.filterText === '') {
+                    return book;
+                } else if (book.book_name.toLowerCase().includes(this.props.filterText.toLowerCase())) {
+                    return book;
+                }
+            }).map((book) => (book.book_releaseYear === year &&
+                out(book)
+            ))}
+        </div>;
+    }
+}
+
+function out(book) {
+    return <div className="element" key={book.book_name}>
+        <div className="element-img"><img src={book.book_photography} alt="img" /></div>
+        <div className="element-title">{book.book_name}</div>
+    </div>;
 }
--- src/components/searchButtons.js
+++ src/components/searchButtons.js
@@ -7,7 +7,7 @@ export default class SearchButtons extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {yearCard: '2021'}
+        this.state = {yearCard: '2021',value: '',}
     }
    
     toggleMe=(value)=>{
@@ -18,15 +18,25 @@ export default class SearchButtons extends React.Component {
         console.log(value);
     }
 
+    inputChangedHandler=(event)=>{
+        this.setState({
+           value: event.target.value
+        })
+
+        console.log(this.state.value);
+    }
+
     render() {
         return(
             <div className="container">
                 <div className="search-buttons-container">
                     <button onClick={()=>this.toggleMe('2021')} className="search-button search-button-2021">2021</button>
                     <button onClick={()=>this.toggleMe('2022')} className="search-button search-button-2022">2022</button>
+
+                    <input className="search-box-input" type="text" placeholder="Search..." onChange={this.inputChangedHandler} />
                 </div>
 
-                <ElementList yearCard={this.state.yearCard}/>
+                <ElementList yearCard={this.state.yearCard} filterText={this.state.value}/>
             </div>
       );
     }
