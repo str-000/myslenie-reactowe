5152b6f8e833a52ff14a57247ecfc3cde1748d96
Łukasz Strama <strama.lukasz54@gmail.com>
2022-03-14 20:33:05 +0100

    ehhh

--- src/components/elementList.js
+++ src/components/elementList.js
@@ -6,30 +6,36 @@ export default class ElementList extends React.Component {
     render() {
         return(
             <div>
-                    {this.props.yearCard==='2021' &&
-                        <div className="element-list-container">
-                            {table.map((book) => (book.book_releaseMonth === 1 && this.outRow(2021)))}
+                    {this.props.yearCard==='2021' && 
+                        <div className="element-list-container"> 
+                            {this.outRow(2021, 1)}{this.outRow(2021, 2)}{this.outRow(2021, 3)}
+                            {this.outRow(2021, 4)}{this.outRow(2021, 5)}{this.outRow(2021, 6)}
+                            {this.outRow(2021, 7)}{this.outRow(2021, 8)}{this.outRow(2021, 9)}
+                            {this.outRow(2021, 10)}{this.outRow(2021, 11)}{this.outRow(2021, 12)}
                         </div>
                     }
 
                     {this.props.yearCard==='2022' && 
                         <div className="element-list-container">
-                            {table.map((book) => (book.book_releaseMonth === 1 && this.outRow(2020)))}
+                            {this.outRow(2022, 1)}{this.outRow(2022, 2)}{this.outRow(2022, 3)}
+                            {this.outRow(2022, 4)}{this.outRow(2022, 5)}{this.outRow(2022, 6)}
+                            {this.outRow(2022, 7)}{this.outRow(2022, 8)}{this.outRow(2022, 9)}
+                            {this.outRow(2022, 10)}{this.outRow(2022, 11)}{this.outRow(2022, 12)}
                         </div>
                     } 
             </div>
       );
     }
 
-    outRow(year) {
-        return <div className="element-list-row">
+    outRow(year, month) {
+        return <div className="element-list-row" key={month}>
             {table.filter(book => {
                 if (this.props.filterText === '') {
                     return book;
                 } else if (book.book_name.toLowerCase().includes(this.props.filterText.toLowerCase())) {
                     return book;
                 }
-            }).map((book) => (book.book_releaseYear === year &&
+            }).map((book) => ((book.book_releaseYear === year && book.book_releaseMonth === month) &&
                 out(book)
             ))}
         </div>;
