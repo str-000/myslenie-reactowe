2bf18d41fbb80f60c8ad6c8570797d4fa1b05928
Łukasz Strama <strama.lukasz54@gmail.com>
2022-03-21 17:41:24 +0100

    static

--- src/components/elementList.js
+++ src/components/elementList.js
@@ -6,38 +6,16 @@ export default class ElementList extends React.Component {
     render() {
         return(
             <div>
-                    {this.props.yearCard==='2021' && 
-                        <div className="element-list-container"> 
-                            {this.outRow(2021, 1)}{this.outRow(2021, 2)}{this.outRow(2021, 3)}
-                            {this.outRow(2021, 4)}{this.outRow(2021, 5)}{this.outRow(2021, 6)}
-                            {this.outRow(2021, 7)}{this.outRow(2021, 8)}{this.outRow(2021, 9)}
-                            {this.outRow(2021, 10)}{this.outRow(2021, 11)}{this.outRow(2021, 12)}
-                        </div>
-                    }
-
-                    {this.props.yearCard==='2022' && 
-                        <div className="element-list-container">
-                            {this.outRow(2022, 1)}{this.outRow(2022, 2)}{this.outRow(2022, 3)}
-                            {this.outRow(2022, 4)}{this.outRow(2022, 5)}{this.outRow(2022, 6)}
-                            {this.outRow(2022, 7)}{this.outRow(2022, 8)}{this.outRow(2022, 9)}
-                            {this.outRow(2022, 10)}{this.outRow(2022, 11)}{this.outRow(2022, 12)}
-                        </div>
-                    } 
+                    <div className="element-list-container"> 
+                        {this.outRow()}
+                    </div>
             </div>
       );
     }
 
-    outRow(year, month) {
-        return <div className="element-list-row" key={month}>
-            {table.filter(book => {
-                if (this.props.filterText === '') {
-                    return book;
-                } else if (book.book_name.toLowerCase().includes(this.props.filterText.toLowerCase())) {
-                    return book;
-                }
-            }).map((book) => ((book.book_releaseYear === year && book.book_releaseMonth === month) &&
-                out(book)
-            ))}
+    outRow() {
+        return <div className="element-list-row">
+            {table.map((book) => (out(book)))}
         </div>;
     }
 }
--- src/components/searchButtons.js
+++ src/components/searchButtons.js
@@ -5,38 +5,17 @@ import ElementList from './elementList';
 
 export default class SearchButtons extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {yearCard: '2021',value: '',}
-    }
-   
-    toggleMe=(value)=>{
-        this.setState({
-            yearCard: value,
-        })
-
-        console.log(value);
-    }
-
-    inputChangedHandler=(event)=>{
-        this.setState({
-           value: event.target.value
-        })
-
-        console.log(this.state.value);
-    }
-
     render() {
         return(
             <div className="container">
                 <div className="search-buttons-container">
-                    <button onClick={()=>this.toggleMe('2021')} className="search-button search-button-2021">2021</button>
-                    <button onClick={()=>this.toggleMe('2022')} className="search-button search-button-2022">2022</button>
+                    <button className="search-button search-button-2021">2021</button>
+                    <button className="search-button search-button-2022">2022</button>
 
-                    <input className="search-box-input" type="text" placeholder="Search..." onChange={this.inputChangedHandler} />
+                    <input className="search-box-input" type="text" placeholder="Search..."/>
                 </div>
 
-                <ElementList yearCard={this.state.yearCard} filterText={this.state.value}/>
+                <ElementList/>
             </div>
       );
     }
